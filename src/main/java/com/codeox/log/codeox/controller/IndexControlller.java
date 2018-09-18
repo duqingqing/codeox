463c2fc37044b3c58bff65773a678e496b392237
duqingqing <dulovefighting.sina.com>
2018-09-18 09:47:26 +0800

    修改controller

--- src/main/java/com/codeox/log/codeox/controller/IndexControlller.java
+++ src/main/java/com/codeox/log/codeox/controller/IndexControlller.java
@@ -1,7 +1,19 @@
 package com.codeox.log.codeox.controller;
 
 
+import com.codeox.log.codeox.commen.password.PasswordTool;
+import com.codeox.log.codeox.commen.result.Result;
+import com.codeox.log.codeox.domain.User;
+import com.codeox.log.codeox.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 
 /**
@@ -13,6 +25,56 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class IndexControlller {
+    @Autowired
+    private UserService userService;
 
+    @PostMapping("/register")
+    public String register(@RequestBody Map<String, String> registerUser) {
+        Result result = userService.addUser(registerUser.get("username"), registerUser.get("password"), registerUser.get("telephone"));
+        if (result.getCode() == 0) {
+            return "hellow";
+        } else {
+            return "register";
+        }
+    }
+
+    @GetMapping("/registerPage")
+    public String registerPage() {
+        return "/register";
+    }
+
+    @GetMapping("/")
+    public ModelAndView index(ModelAndView modelAndView) {
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(ModelAndView modelAndView, HttpSession httpSession, String username, String password) {
+        try {
+            User user = userService.fingUserByUserName(username);
+            String encodeUserPasswprd = user.getPassword();
+            PasswordTool passwordTool = new PasswordTool();
+            String decodeUserpassword = passwordTool.decodePassword(encodeUserPasswprd);
+            if (!password.equals(decodeUserpassword)) {
+                modelAndView.addObject("error", "密码不正确");
+                modelAndView.setViewName("login");
+            } else {
+                httpSession.setAttribute("user", username);
+                modelAndView.setViewName("/index");
+            }
+        } catch (NullPointerException nullPointerException) {
+            modelAndView.addObject("error", "用户名不存在");
+            modelAndView.setViewName("register");
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/quit")
+    public ModelAndView quit(ModelAndView modelAndView, HttpSession httpSession) {
+        httpSession.removeAttribute("user");
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
 
 }
