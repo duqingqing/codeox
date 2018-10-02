35c5f7ad3e1e4ccfb573bb39fcf4a73fd9e14e3f
duqingqing <dulovefighting.sina.com>
2018-10-02 23:53:23 +0800

    添加管理员首页

--- src/main/java/com/codeox/log/codeox/controller/AdminControlller.java
+++ src/main/java/com/codeox/log/codeox/controller/AdminControlller.java
@@ -3,14 +3,14 @@ package com.codeox.log.codeox.controller;
 
 import com.codeox.log.codeox.commen.password.PasswordTool;
 import com.codeox.log.codeox.domain.Admin;
-import com.codeox.log.codeox.domain.User;
 import com.codeox.log.codeox.service.AdminService;
-import org.hibernate.Session;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,27 +26,34 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/admin")
+@Slf4j
 public class AdminControlller {
     @Autowired
     private AdminService adminService;
 
     /**
-     * @Description: 管理员首页,通过session判断登入状态
+     * @Description: 管理员首页, 通过session判断登入状态
      * @Param: modelAndView
      * @ParamhttpSession
      * @return:ModelAndView
      * @Date: 2018/10/2 0002
      */
-    @GetMapping("/")
-    public ModelAndView index(ModelAndView modelAndView, HttpSession httpSession) {
+    @GetMapping({"/", "/index", "/login"})
+    public String index(ModelAndView modelAndView, HttpSession httpSession) {
         Object object = httpSession.getAttribute("name");
+        String result = "";
         if (object == null) {
-            modelAndView.setViewName("admin/login");
+            result = "/admin/login";
         } else {
-            modelAndView.setViewName("admin/index");
+            result = "/admin/index";
         }
-        return modelAndView;
+        return result;
     }
+
+//    @GetMapping("/login")
+//    public String login() {
+//        return "/admin/login";
+//    }
 
     /**
      * @Description: 管理员登入
@@ -54,7 +61,7 @@ public class AdminControlller {
      * @return:
      * @Date: 2018/10/2 0002
      */
-    @PostMapping(value = "/login")
+    @RequestMapping (value = "/login/action",method = RequestMethod.POST)
     public ModelAndView login(HttpServletRequest request) {
         //解密工具
         PasswordTool passwordTool = new PasswordTool();
@@ -62,12 +69,16 @@ public class AdminControlller {
         modelAndView.setViewName("admin/login");
         HttpSession session = request.getSession();
         //从表单获取名字和密码
-        String name = request.getParameter("name");
+        String name = request.getParameter("username");
         String password = request.getParameter("password");
+        log.info("【登入名】" + name);
+        log.info("【密码】" + password);
+
         //获取admin实例
         Admin admin = adminService.findByName(name);
         String adminPassword = admin.getPassword();
         String decodePassword = passwordTool.decodePassword(adminPassword);
+        log.info("【数据库密码】" + decodePassword);
         if (decodePassword.equals(password)) {
             //添加登入信息
             session.setAttribute("name", name);
